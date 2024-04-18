4cfad37aa49ff8318c859d66cb22c28cffe5e65d
Park JunYong <dyden119@gmail.com>
2024-04-18 18:13:18 +0900

    커밋 테스트

--- src/pages/ActivityDetails/index.tsx
+++ src/pages/ActivityDetails/index.tsx
@@ -1,19 +1,28 @@
 import { useParams } from 'react-router-dom';
+import { useModal } from '../../components/modal/useModal';
 
 import toast from '../../utils/toast';
+import Temp from '../../components/temp';
 
 export default function ActivityDetails() {
   const { id } = useParams();
+  const { Modal, openModal, closeModal } = useModal();
 
   return (
-    <main>
-      {id}번 체험상세 페이지 입니다.
-      <br />
-      <button onClick={() => toast.success('성공 토스트 입니다.')}>성공토스트</button>
-      <br />
-      <button onClick={() => toast.warning('경고 토스트 입니다.')}>경고토스트</button>
-      <br />
-      <button onClick={() => toast.error('에러 토스트 입니다.')}>에러토스트</button>
-    </main>
+    <>
+      <main>
+        {id}번 체험상세 페이지 입니다.
+        <br />
+        <button onClick={() => toast.success('성공 토스트 입니다.')}>성공토스트</button>
+        <br />
+        <button onClick={() => toast.warning('경고 토스트 입니다.')}>경고토스트</button>
+        <br />
+        <button onClick={() => toast.error('에러 토스트 입니다.')}>에러토스트</button>
+        <button onClick={() => openModal('a')}>모달</button>
+      </main>
+      <Modal type="a">
+        <Temp closeModal={closeModal} />
+      </Modal>
+    </>
   );
 }
