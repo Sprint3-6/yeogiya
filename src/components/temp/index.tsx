c310aeb65997b58d0f73a00125a57fb9682095b8
Park JunYong <dyden119@gmail.com>
2024-04-19 01:54:36 +0900

    chore: 모달,로그인,인터셉터 테스트

--- src/components/temp/index.tsx
+++ src/components/temp/index.tsx
@@ -1,5 +1,8 @@
 import './style.scss';
 
+// 모달 테스트용 임시 컴포넌트 입니다
+// 추후에 삭제하도록 하겠습니다!
+
 type Type = {
   closeModal: () => void;
 };
--- src/pages/ActivityDetails/index.tsx
+++ src/pages/ActivityDetails/index.tsx
@@ -1,12 +1,31 @@
 import { useParams } from 'react-router-dom';
 import { useModal } from '../../components/modal/useModal';
+import instance from '../../api/instance/defaultInstance';
+import { BASE_URL } from '../../api/constants/url';
 
 import toast from '../../utils/toast';
 import Temp from '../../components/temp';
 
+import './style.scss';
+
 export default function ActivityDetails() {
   const { id } = useParams();
   const { Modal, openModal, closeModal } = useModal();
+
+  const login = async () => {
+    const userInfo = await instance.post(`${BASE_URL}auth/login`, {
+      email: 'jyp1@jyp.com',
+      password: '12345678',
+    });
+    console.log(userInfo);
+    localStorage.setItem('accessToken', userInfo.data.accessToken);
+    localStorage.setItem('refreshToken', userInfo.data.refreshToken);
+  };
+
+  const viewList = async () => {
+    const myList = await instance.get(`${BASE_URL}my-activities?size=20`);
+    console.log(myList);
+  };
 
   return (
     <>
@@ -19,7 +38,11 @@ export default function ActivityDetails() {
         <br />
         <button onClick={() => toast.error('에러 토스트 입니다.')}>에러토스트</button>
         <button onClick={() => openModal('a')}>모달</button>
+        <button onClick={viewList}>내 체험리스트 조회</button>
       </main>
+      <button className="temp-login" onClick={login}>
+        로그인
+      </button>
       <Modal type="a">
         <Temp closeModal={closeModal} />
       </Modal>
