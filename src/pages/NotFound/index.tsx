246b4094003dec39776a94c837a47f0ac9b06ef6
Jinah <monsatfri@gmail.com>
2024-04-19 19:18:31 +0900

    design: 클래스명 camelCase -> kebab-case 변경

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,11 +2,11 @@ import './style.scss';
 
 export default function NotFound() {
   return (
-    <main className="notFoundWrapper">
-      <img src="/public/assets/logos/logo-notFound.svg" alt="로고" className="notFoundLogo" />
-      <div className="notFoundTextContainer">
-        <div className="notFoundErrorText">404 Not Found</div>
-        <div className="notFoundErrorTextContent">요청하신 페이지를 찾을 수 없습니다.</div>
+    <main className="not-found-wrapper">
+      <img src="/public/assets/logos/logo-notFound.svg" alt="로고" className="not-found-logo" />
+      <div className="not-found-text-container">
+        <div className="not-found-error-text">404 Not Found</div>
+        <div className="not-found-error-text-content">요청하신 페이지를 찾을 수 없습니다.</div>
       </div>
     </main>
   );
