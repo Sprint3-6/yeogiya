9202e45ee31ad497b936522fcf6da0a40c106c9c
unknown <kimksy27@gmail.com>
2024-04-23 02:44:24 +0900

    Feat : 드롭다운 컴포넌트 선택 추가

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,3 +1,68 @@
-export default function Dropdown() {
-  return;
-}
+import { useState } from 'react';
+import './style.scss';
+
+export const DropDown = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [select, setSelect] = useState(null);
+
+  // 드롭다운 타이틀
+  const title = props.title;
+  const arrowUp = props.arrowUp;
+  const arrowDown = props.arrowDown;
+  const array = props.children;
+
+  // 드롭다운 아이템
+  // 가격이 낮은 순, 가격이 높은 순, 예약 신청, 예약 취소
+
+  const handleSelect = (e) => {
+    const selectOption = e.target.outerText;
+    setSelect(selectOption);
+    console.log(select);
+  };
+
+  const handleTitle = (arrow) => {
+    return (
+      <div className="dropdown-title">
+        <div>{select === null ? title : select}</div>
+        <div>{arrow}</div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div
+        className="dropdown-select"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        {
+          isOpen ? handleTitle(arrowUp) : handleTitle(arrowDown)
+
+          // <div className="dropdown-title">
+          //   <div>{title}</div>
+          //   <div>{arrowDown}</div>
+          // </div>
+        }
+      </div>
+      <ul className="dropdown-option-list">
+        {/* {isOpen &&
+          array.map((e) => (
+            <li className="dropdown-option" onClick={console.log(e.props.children[0])}>
+              {e.props.children}
+            </li>
+          ))} */}
+        {isOpen && (
+          <li className="dropdown-option" onClick={handleSelect}>
+            {array}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export const DropdownItem = (props) => {
+  return <div>{props.children}</div>;
+};
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,3 +1,25 @@
+import { DropDown, DropdownItem } from '../../components/DropDown';
+
 export default function SignIn() {
-  return <main>로그인 페이지 입니다.</main>;
+  return (
+    <main>
+      로그인 페이지 입니다.
+      <DropDown title={'가격'} arrowUp="🔺" arrowDown="🔻">
+        <DropdownItem>낮은 순</DropdownItem>
+        <DropdownItem>높은 순</DropdownItem>
+      </DropDown>
+      <DropDown title={'카테고리'} arrowUp="^" arrowDown="v">
+        <DropdownItem>문화 예술</DropdownItem>
+        <DropdownItem>식음료</DropdownItem>
+        <DropdownItem>스포츠</DropdownItem>
+        <DropdownItem>투어</DropdownItem>
+        <DropdownItem>관광</DropdownItem>
+        <DropdownItem>웰빙</DropdownItem>
+      </DropDown>
+      <DropDown arrowUp=":" arrowDown=":">
+        <DropdownItem>수정하기</DropdownItem>
+        <DropdownItem>삭제하기</DropdownItem>
+      </DropDown>
+    </main>
+  );
 }
