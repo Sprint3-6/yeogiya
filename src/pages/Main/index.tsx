59d72d8e4db8eab1d39d7c5833e8e6f1f6546ba0
Park JunYong <dyden119@gmail.com>
2024-04-22 20:41:01 +0900

    chore: 임시 로그인버튼

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
+import { BASE_URL } from '../../api/constants/url';
 import instance from '../../api/instance/defaultInstance';
+import Loading from '../Loading';
 
 import './style.scss';
+import { useState } from 'react';
 
 interface ErrorType {
   response: {
@@ -11,35 +14,41 @@ interface ErrorType {
   };
 }
 
-const handleTempLogin = async () => {
-  const id = prompt('이메일');
-  try {
-    if (id) {
-      const password = prompt('비밀번호');
-      if (password) {
-        const response = await instance.post('https://sp-globalnomad-api.vercel.app/3-6/auth/login', {
-          email: id,
-          password: password,
-        });
-        alert(`${id}로 로그인 되었습니다.(자세한건 콘솔창 참조)`);
-        console.log(response);
-        localStorage.setItem('accessToken', response.data.accessToken);
-        localStorage.setItem('refreshToken', response.data.refreshToken);
+export default function MainPage() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleTempLogin = async () => {
+    const id = prompt('이메일');
+    try {
+      if (id) {
+        const password = prompt('비밀번호');
+        if (password) {
+          setIsLoading(true);
+          const response = await instance.post(`${BASE_URL}auth/login`, {
+            email: id,
+            password: password,
+          });
+          alert(`${id}로 로그인 되었습니다.(자세한건 콘솔창)`);
+          console.log(response);
+          localStorage.setItem('accessToken', response.data.accessToken);
+          localStorage.setItem('refreshToken', response.data.refreshToken);
+        } else {
+          alert('취소함');
+        }
       } else {
         alert('취소함');
       }
-    } else {
-      alert('취소함');
+    } catch (err) {
+      const error = err as ErrorType;
+      alert(error.response.data.message);
+    } finally {
+      setIsLoading(false);
     }
-  } catch (err) {
-    const error = err as ErrorType;
-    alert(error.response.data.message);
-  }
-};
+  };
 
-export default function MainPage() {
   return (
     <main>
+      {isLoading && <Loading />}
       메인페이지 입니다.
       <br />
       <Link to={'sign-in'}>로그인</Link>
