4d5a4ae1e44c84f2508c6718420fc954e29d9d86
괴도소연\박소연 <rnlduq157@naver.com>
2024-04-24 23:47:40 +0900

    feat: header 로그인 여부에 따른 버튼 조건부 구현

---
+++ src/components/Header/components/HeaderButtons.tsx
@@ -0,0 +1,52 @@
+import './headerButton.scss';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { myInfo } from '@/redux/myInfoSlice';
+import { useModal } from '@/hooks/useModal/useModal';
+import MyNotifications from '@/components/MyNotifications';
+import HeaderDropDown from './HeaderDropdown';
+
+export default function HeaderButtons() {
+  const userData = useSelector(myInfo);
+  const { Modal, openModal, closeModal } = useModal();
+  const defaultProfileImage = '/public/assets/images/profile-default.png';
+
+  return (
+    <>
+      {userData.id !== null ? (
+        <div className="header-login">
+          <img
+            src="/public/assets/icons/icon-notification.svg"
+            alt="알림 아이콘"
+            onClick={() => openModal('header-notifications')}
+            className="header-notifications-icon"
+          />
+          <img src="/public/assets/icons/icon-vector.svg" alt="아이콘 구분선" />
+          <div className="header-login-user" onClick={() => openModal('header-dropdown')}>
+            <img src={userData.profileImageUrl || defaultProfileImage} alt="프로필 사진" />
+            <span>{userData.nickname}</span>
+          </div>
+          <Modal name="header-notifications">
+            <div className="header-notifications-container">
+              <MyNotifications onClose={closeModal} />
+            </div>
+          </Modal>
+          <Modal name="header-dropdown">
+            <div className="header-dropdown-container">
+              <HeaderDropDown />
+            </div>
+          </Modal>
+        </div>
+      ) : (
+        <div className="header-buttons">
+          <Link to="/sign-in">
+            <button className="header-button">로그인</button>
+          </Link>
+          <Link to="/sign-up">
+            <button className="header-button">회원가입</button>
+          </Link>
+        </div>
+      )}
+    </>
+  );
+}
