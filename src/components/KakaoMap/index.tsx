262781a07d0177daa677d01363b2a86aaf851f0e
Park JunYong <dyden119@gmail.com>
2024-04-26 00:21:03 +0900

    chore: 지도 확대수준 수정

--- src/components/KakaoMap/index.tsx
+++ src/components/KakaoMap/index.tsx
@@ -13,7 +13,7 @@ export default function KakaoMap() {
     const options = {
       //지도를 생성할 때 필요한 기본 옵션
       center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 8, //지도의 레벨(확대, 축소 정도)
+      level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
     //@ts-ignore
