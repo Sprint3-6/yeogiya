73f235fc2e463a323dc8ce4c4039e884324909cc
괴도소연\박소연 <rnlduq157@naver.com>
2024-04-27 04:41:38 +0900

    fix: merge conflict 해결

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -29,24 +29,6 @@ export default function MainPage() {
 
   return (
     <main>
-      <h1>메인 페이지</h1>
-      <nav>
-        <Link to="sign-in">로그인</Link>
-        <br />
-        <Link to="sign-up">회원가입</Link>
-        <br />
-        <Link to="mypage">내정보</Link>
-        <br />
-        <Link to="mypage/reservation">예약내역</Link>
-        <br />
-        <Link to="mypage/admin/status">예약현황</Link>
-        <br />
-        <Link to="mypage/admin">내 공간 관리</Link>
-        <br />
-        <Link to="space/718">공간 상세</Link>
-      </nav>
-      {data?.activities && <SpaceCardList spaces={data.activities} />}
-      <Pagination totalCount={data?.totalCount} size={6} />
       메인페이지 입니다.
       <br />
       <Link to={'sign-in'}>로그인</Link>
@@ -64,6 +46,8 @@ export default function MainPage() {
       <Link to={'space/735'}>공간상세(내꺼)</Link>
       <br />
       <Link to={'space/704'}>공간상세(내꺼아님)</Link>
+      {data?.activities && <SpaceCardList spaces={data.activities} />}
+      <Pagination totalCount={data?.totalCount} size={6} />
     </main>
   );
 }
