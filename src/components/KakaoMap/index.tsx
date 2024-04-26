61b13ed3f89e3594d156c8b3d41383c4fd2787bb
Park JunYong <dyden119@gmail.com>
2024-04-26 13:36:47 +0900

    chore: 지도에 스카이뷰 추가

--- src/components/KakaoMap/index.tsx
+++ src/components/KakaoMap/index.tsx
@@ -15,9 +15,10 @@ export default function KakaoMap() {
       center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
+    let mapTypeControl = new window.kakao.maps.MapTypeControl();
 
-    //@ts-ignore
     const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT);
   }, []);
 
   return <div id="kakao-map"></div>;
