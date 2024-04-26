2191000940e8451c147b0f2b6a52b0331a36933b
괴도소연\박소연 <rnlduq157@naver.com>
2024-04-27 04:25:20 +0900

    test: 기능 테스트를 위해 임시로 데이터 호출

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,25 +1,54 @@
 import { Link } from 'react-router-dom';
-
 import './style.scss';
+import { useState, useEffect } from 'react';
+import instance from '@/api/instance/defaultInstance';
+import { BASE_URL } from '@/api/constants/url';
+import { Spaces } from './types/spaces-type';
+import SpaceCardList from './components/SpaceCardList';
+import Pagination from '@/components/Pagination';
+interface DataType {
+  activities: Spaces[];
+  totalCount: number;
+}
 
 export default function MainPage() {
+  const [data, setData] = useState<DataType | null>(null);
+
+  useEffect(() => {
+    const getSpaces = async () => {
+      try {
+        const res = await instance.get(`${BASE_URL}activities?method=offset`);
+        setData(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getSpaces();
+  }, []);
+
   return (
     <main>
-      메인페이지 입니다.
-      <br />
-      <Link to={'sign-in'}>로그인</Link>
-      <br />
-      <Link to={'sign-up'}>회원가입</Link>
-      <br />
-      <Link to={'mypage'}>내정보</Link>
-      <br />
-      <Link to={'mypage/reservation'}>예약내역</Link>
-      <br />
-      <Link to={'mypage/admin/status'}>예약현황</Link>
-      <br />
-      <Link to={'mypage/admin'}>내 공간 관리</Link>
-      <br />
-      <Link to={'space/718'}>공간상세</Link>
+      <h1>메인 페이지</h1>
+      <nav>
+        <Link to="sign-in">로그인</Link>
+        <br />
+        <Link to="sign-up">회원가입</Link>
+        <br />
+        <Link to="mypage">내정보</Link>
+        <br />
+        <Link to="mypage/reservation">예약내역</Link>
+        <br />
+        <Link to="mypage/admin/status">예약현황</Link>
+        <br />
+        <Link to="mypage/admin">내 공간 관리</Link>
+        <br />
+        <Link to="space/718">공간 상세</Link>
+      </nav>
+
+      {data?.activities && <SpaceCardList spaces={data.activities} />}
+
+      <Pagination totalCount={data?.totalCount} size={6} />
     </main>
   );
 }
