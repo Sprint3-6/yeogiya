5d3caefe2ff5f04fa05da8427767d5681d57e151
Park JunYong <dyden119@gmail.com>
2024-04-27 01:54:24 +0900

    feat: 공간상세 페이지 후기부분까지 구현

--- src/api/getSpaceDetail.ts
+++ src/api/getSpaceDetail.ts
@@ -3,7 +3,6 @@ import instance from './instance/defaultInstance';
 
 async function getSpaceDetail(id: string | undefined) {
   const detail = await instance.get(`${BASE_URL}activities/${id}`);
-  console.log(detail.data);
   return detail.data;
 }
 
