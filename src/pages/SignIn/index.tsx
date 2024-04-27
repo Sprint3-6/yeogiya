e0999934bdab5a41eb2cc5b38c8dc49998021770
unknown <kimksy27@gmail.com>
2024-04-27 21:52:28 +0900

    Feat : 로고 클릭시 메인 화면 이동

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
 import './style.scss';
 
 export default function SignIn() {
-  return <main>로그인 페이지 입니다.</main>;
+  return (
+    <main>
+      로그인 페이지 입니다.
+      <div>
+        <Link to="/">
+          <img src="/assets/logos/logo-big.svg" alt="메인 이동" />
+        </Link>
+      </div>
+      <div>이메일</div>
+      <div>비밀번호</div>
+      <div>버튼</div>
+      <div>회원이 아니신가요? 회원가입하기</div>
+    </main>
+  );
 }
