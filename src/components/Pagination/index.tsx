0a5e567a466b6b12fe274bca39bd34934238824b
괴도소연\박소연 <rnlduq157@naver.com>
2024-04-30 02:42:55 +0900

    refactor: 페이지네이션 프롭 추가

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -4,16 +4,15 @@ import PageButton from './components/PageButton';
 import './style.scss';
 
 interface PaginationProps {
-  totalCount?: number;
-  size: number;
+  totalCount?: number; // 전체 데이터 개수
+  size: number; // 한 페이지당 보여줄 데이터 개수
+  page: number; // 현재 페이지 번호
+  setPage: (value: number) => void; // 현재 페이지 번호 set 함수
 }
 
-export default function Pagination({ totalCount, size }: PaginationProps) {
+export default function Pagination({ totalCount, size, page, setPage }: PaginationProps) {
   const count = totalCount ?? 0;
-  const { page, currentPageGroup, hasPrev, hasNext, handlePageClick, handlePrevClick, handleNextClick } = usePaginator(
-    count,
-    size,
-  );
+  const { currentPageGroup, hasPrev, hasNext } = usePaginator(count, size, page);
 
   if (count === 0) {
     return null;
@@ -21,11 +20,11 @@ export default function Pagination({ totalCount, size }: PaginationProps) {
 
   return (
     <div className="pagination-wrapper">
-      <ArrowButton isDisabled={!hasPrev} direction="prev" onClick={handlePrevClick} />
-      {currentPageGroup.map((index) => (
-        <PageButton key={index} page={index} onClick={() => handlePageClick(index)} isDisabled={page !== index} />
+      <ArrowButton isDisabled={!hasPrev} direction="prev" onClick={() => setPage(page - 1)} />
+      {currentPageGroup.map((num) => (
+        <PageButton key={num} page={num} onClick={() => setPage(num)} isDisabled={page !== num} />
       ))}
-      <ArrowButton isDisabled={!hasNext} direction="next" onClick={handleNextClick} />
+      <ArrowButton isDisabled={!hasNext} direction="next" onClick={() => setPage(page + 1)} />
     </div>
   );
 }
--- src/hooks/usePaginator/usePaginator.ts
+++ src/hooks/usePaginator/usePaginator.ts
@@ -1,30 +1,14 @@
-/*TODO
-- page: 버튼 클릭 몇페이지인지? (쿼리)
-- size: 페이지 당 보여줄 데이터 개수, 반응형으로 개수가 달라짐 (쿼리)
-- category, keyword, sort (메인페이지)
-- 보여줄 페이지 배열 (1~5) (6~10) 업데이트
-- 전체 페이지 개수: totalCount / size
-- 이전, 다음 버튼: 활성 / 비활성화 (이전 또는 다음 페이지가 있는지?), 몇페이지인지 업데이트,,
-- url 프롭으로 전달 받고 API 호출?
-- method는 offset
-*/
-
 import { useEffect, useState } from 'react';
 
 const PAGE_ARRAY_LIMIT = 5;
 
 interface PaginationUtilProps {
-  page: number;
-  currentPageGroup: number[] | [];
-  hasPrev: boolean;
-  hasNext: boolean;
-  handlePageClick: (index: number) => void;
-  handlePrevClick: () => void;
-  handleNextClick: () => void;
+  currentPageGroup: number[] | []; // 현재 보여질 페이지 배열 (1~5, 6~10, ...)
+  hasPrev: boolean; // 이전 버튼 비활성화 여부
+  hasNext: boolean; // 다음 버튼 비활성화 여부
 }
 
-export default function usePaginator(totalCount: number, size: number): PaginationUtilProps {
-  const [page, setPage] = useState(1);
+export default function usePaginator(totalCount: number, size: number, page: number): PaginationUtilProps {
   const [currentPageGroup, setCurrentPageGroup] = useState<number[]>([]);
   const [hasPrev, setHasPrev] = useState<boolean>(false);
   const [hasNext, setHasNext] = useState<boolean>(false);
@@ -43,25 +27,8 @@ export default function usePaginator(totalCount: number, size: number): Paginati
     for (let i = startPage; i <= endPage; i++) {
       pageNumbers.push(i);
     }
-    console.log(`현재 페이지: ${page}`);
     setCurrentPageGroup(pageNumbers);
   }, [page, totalCount, size]);
 
-  const handlePrevClick = () => {
-    if (hasPrev) {
-      setPage((prev) => prev - 1);
-    }
-  };
-
-  const handlePageClick = (index: number) => {
-    setPage(index);
-  };
-
-  const handleNextClick = () => {
-    if (hasNext) {
-      setPage((prev) => prev + 1);
-    }
-  };
-
-  return { page, currentPageGroup, hasPrev, hasNext, handlePageClick, handlePrevClick, handleNextClick };
+  return { currentPageGroup, hasPrev, hasNext };
 }
