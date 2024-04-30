2a55e59fdea18dad760cb89882c20feb52e69472
Jinah <monsatfri@gmail.com>
2024-04-30 15:07:08 +0900

    fix: img 경로 수정하여 경고 제거

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,7 +3,7 @@ import './style.scss';
 export default function NotFound() {
   return (
     <main className="not-found-wrapper">
-      <img src="/public/assets/logos/logo-notFound.svg" alt="로고" className="not-found-logo" />
+      <img src="/assets/logos/logo-notFound.svg" alt="로고" className="not-found-logo" />
       <div className="not-found-text-container">
         <div className="not-found-error-text">404 Not Found</div>
         <div className="not-found-error-text-content">요청하신 페이지를 찾을 수 없습니다.</div>
