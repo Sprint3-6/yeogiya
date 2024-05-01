670b9943d7115d509f6abe0ed82d2c87650b0cfb
괴도소연\박소연 <rnlduq157@naver.com>
2024-05-02 05:25:09 +0900

    rename: 공간 api 타입 파일 정리

---
+++ src/api/getSpaces.ts
@@ -0,0 +1,16 @@
+import { BASE_URL } from './constants/url';
+import instance from './instance/defaultInstance';
+import { GetSpacesParam } from './types/activities';
+
+async function getSpaces({ cursorId, category, method, keyword, sort, page = 1, size }: GetSpacesParam) {
+  let path = '';
+  if (cursorId) path += `&cursorId=${cursorId}`;
+  if (category) path += `&category=${category}`;
+  if (keyword) path += `&keyword=${keyword}`;
+  if (sort) path += `&sort=${sort}`;
+
+  const spaces = await instance.get(`${BASE_URL}activities?method=${method}&page=${page}&size=${size}${path}`);
+  return spaces.data;
+}
+
+export default getSpaces;
---
+++ src/api/types/activities.ts
@@ -0,0 +1,25 @@
+export type Category = '문화 · 예술' | '식음료' | '스포츠' | '투어' | '관광' | '웰빙' | '';
+
+export interface Spaces {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: Category;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  rating: number;
+  reviewCount: number;
+  createdAt: string;
+  updatedAt: string;
+}
+export interface GetSpacesParam {
+  method: 'offset' | 'cursor';
+  cursorId?: number | null;
+  category?: Category;
+  keyword?: string;
+  sort?: 'most_reviewed' | 'price_asc' | 'price_desc' | 'latest';
+  page?: number;
+  size?: number;
+}
