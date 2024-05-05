f85eb4008d90d71152d6b19ddf9db1d95cce5bef
괴도소연\박소연 <rnlduq157@naver.com>
2024-05-06 05:39:18 +0900

    feat: 검색바 삭제 아이콘 검색값 조건 추가

--- src/pages/Main/components/SearchBar.tsx
+++ src/pages/Main/components/SearchBar.tsx
@@ -1,5 +1,5 @@
-import Button from '@/components/Button';
 import './searchBar.scss';
+import { FormEvent, MouseEvent, ChangeEvent } from 'react';
 
 /*TODO 검색창 문구 변경
 [] 검색하기 버튼 누를 때 검색됨
@@ -10,33 +10,44 @@ import './searchBar.scss';
 [] 검색된 결과 없을 때
 [] '검색어' 으로 검색한 결과 표시 + 결과 개수
 */
-/*
+
 interface SearchBarProps {
   searchValue: string;
-  handleSearchSpace: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSearchSubmit: (event: FormEvent<HTMLFormElement>, text?: string) => void;
-}*/
+  handleSearchText: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleSearchSubmit: (
+    e: FormEvent<HTMLFormElement> | MouseEvent<HTMLDivElement> | MouseEvent<HTMLButtonElement>,
+    text?: string,
+  ) => void;
+}
 
-export default function SearchBar() {
+function SearchBar({ searchValue, handleSearchText, handleSearchSubmit }: SearchBarProps) {
   return (
     <div className="search-bar-wrapper">
       <h1>어떤 공간을 원하시나요?</h1>
       <div className="search-bar-search-box">
-        <form className="search-bar-input-form">
-          <img src="/favicon.svg" />
+        <form className="search-bar-input-form" onSubmit={handleSearchSubmit}>
+          <img src="/favicon.svg" alt="favicon" />
           <input
             type="search"
-            //value={searchValue}
-            //onChange={handleSearchSpace}
+            value={searchValue}
+            onChange={handleSearchText}
             className="search-bar-input"
             placeholder="내가 원하는 공간은"
           />
-          <button className="search-bar-delete-search-button">
-            <img src="/assets/icons/icon-closed.svg" />
-          </button>
+          {searchValue && (
+            <button
+              className="search-bar-delete-search-button"
+              onClick={(e) => handleSearchSubmit(e, '')}
+              type="button"
+            >
+              <img src="/assets/icons/icon-closed.svg" alt="icon-closed" />
+            </button>
+          )}
         </form>
-        <Button type="submit">검색하기</Button>
+        <button onClick={(e: MouseEvent<HTMLButtonElement>) => handleSearchSubmit(e, searchValue)}>검색하기</button>
       </div>
     </div>
   );
 }
+
+export default SearchBar;
