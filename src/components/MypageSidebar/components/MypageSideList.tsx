3ac80f9fba22a1ae5374702c1278965ba9504883
kimsuns <152264841+kimsuns@users.noreply.github.com>
2024-05-06 01:21:08 +0900

    Update MypageSideList.tsx

--- src/components/MypageSidebar/components/MypageSideList.tsx
+++ src/components/MypageSidebar/components/MypageSideList.tsx
@@ -31,18 +31,6 @@ export const MyPageSideList = ({ image, clickImage, link, text }: mypagesideProp
       setIsIcon(image);
     }
   }, [location.pathname]);
-  // useLocation(); // 사용하기
-  // 이걸로 패스네임 알 수 있음
-
-  // if (window.location.pathname === `/${link}`) {
-  //   handleMouseEnter();
-  // } else {
-  //   handleMouseLeave();
-  // }
-  // useEffect(() => {}, [window.location.pathname]);
-  // put // 모든 데이터 묶은 데이터
-  // patch // 부분 데이터 변경
-  // 리퀘스트 바디
 
   return (
     <Link to={link}>
