bb227c703cb39239f28b3319f922c45bb2504dd3
괴도소연\박소연 <rnlduq157@naver.com>
2024-05-06 05:36:31 +0900

    feat: 예약내역 페이지 드롭다운 추가

--- src/pages/MyReservation/index.tsx
+++ src/pages/MyReservation/index.tsx
@@ -1,3 +1,19 @@
+import { DropDown, DropdownItem } from '@/components/Dropdown';
+
+//TODO 체험완료 문구 변경
+
 export default function MyReservation() {
-  return <main>예약내역 페이지 입니다.</main>;
+  return (
+    <>
+      <h1>예약 내역</h1>
+      <DropDown id="my-reservation-list" title="예약 상태">
+        <DropdownItem value="all">전체</DropdownItem>
+        <DropdownItem value="reservation-pending">예약 신청</DropdownItem>
+        <DropdownItem value="reservation-canceled">예약 취소</DropdownItem>
+        <DropdownItem value="reservation-confirmed">예약 승인</DropdownItem>
+        <DropdownItem value="reservation-declined">예약 거절</DropdownItem>
+        <DropdownItem value="reservation-completed">방문 완료</DropdownItem>
+      </DropDown>
+    </>
+  );
 }
