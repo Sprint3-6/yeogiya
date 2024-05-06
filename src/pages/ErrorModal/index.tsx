00233805493d9ba37f1a02a505b56dd8db214a48
Jinah <monsatfri@gmail.com>
2024-05-06 16:59:52 +0900

    fix: 타입 오류 수정 및 img 경로 내 public 작성으로 인한 경고문 제거

--- src/pages/ErrorModal/index.tsx
+++ src/pages/ErrorModal/index.tsx
@@ -8,7 +8,7 @@ interface ErrorModalProps {
 export default function ErrorModal({ onClose }: ErrorModalProps) {
   return (
     <div className="error-wrapper">
-      <img src="/public/assets/logos/logo-notFound.svg" alt="로고" className="error-logo" />
+      <img src="/assets/logos/logo-notFound.svg" alt="로고" className="error-logo" />
       <div className="error-title">ERROR</div>
       <div className="error-message">빈 공간입니다. </div>
       <Button children="뒤로 가기" type="button" onClick={onClose} className="button-black error-button" />
