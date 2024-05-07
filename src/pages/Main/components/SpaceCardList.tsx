c5c74eced9f752ca85a286268138b00d7ba785ed
괴도소연\박소연 <rnlduq157@naver.com>
2024-05-07 22:10:00 +0900

    feat: 메인페이지 해당 공간 없을 때 조건부 렌더링 추가 및 디자인 수정

--- src/pages/Main/components/SpaceCardList.tsx
+++ src/pages/Main/components/SpaceCardList.tsx
@@ -29,39 +29,55 @@ export default function SpaceCardList({
 
   //TODO 드롭다운 밸류 설정
   return (
-    <>
+    <section className="main-space-card-list-container">
       {/* searchValue가 없을 때만 드롭다운과 카테고리 버튼을 렌더링 */}
       {searchResult === '' && (
-        <div className="space-selected-list-wrapper">
-          <div className="space-categories-wrapper">
-            {CATEGORIES.map((category) => (
-              <Button key={category} className="button-black" onClick={() => handleCategoryClick(category)}>
-                {categoryFilter(category)}
-              </Button>
-            ))}
-          </div>
-          <DropDown id="space-list-dropdown" title="가격" onClickItem={handleSortSpaces}>
-            <DropdownItem value="high">높은 순</DropdownItem>
-            <DropdownItem value="row">낮은 순</DropdownItem>
-          </DropDown>
+        <div className="space-categories-wrapper">
+          {CATEGORIES.map((category) => (
+            <Button key={category} className="button-white" onClick={() => handleCategoryClick(category)}>
+              {categoryFilter(category)}
+            </Button>
+          ))}
         </div>
       )}
       {/* 검색어가 있으면 검색어를, 그렇지 않으면 카테고리 이름을 표시 */}
-      <div className="space-list-search-result">
+      <div className="space-list-selected-wrapper">
         {searchResult !== '' ? (
-          <>
+          <div className="space-list-search-result">
             {searchResult}
             <span>으로 검색한 결과입니다</span>
-          </>
+          </div>
         ) : (
-          categoryFilter(selectedCategory || '')
+          <>
+            <h3>{categoryFilter(selectedCategory || '')}</h3>
+            <div className="space-list-sort-dropdown">
+              <DropDown
+                id="space-list-dropdown"
+                title="가격"
+                arrowUp={'▲'}
+                arrowDown={'▼'}
+                onClickItem={handleSortSpaces}
+              >
+                <DropdownItem value="high">높은 순</DropdownItem>
+                <DropdownItem value="row">낮은 순</DropdownItem>
+              </DropDown>
+            </div>
+          </>
         )}
       </div>
-      <div className="space-card-list-container">
-        {spaces.map((space) => (
-          <SpaceCard key={space.id} item={space} />
-        ))}
-      </div>
-    </>
+      {/* spaces 데이터가 없을 때 조건부 렌더링 */}
+      {spaces.length === 0 ? (
+        <div className="not-found-file-box">
+          <img src="/assets/images/not-found-file.svg" alt="관리할 방이 없습니다" />
+          <span>해당하는 공간이 없어요</span>
+        </div>
+      ) : (
+        <div className="space-card-list-container">
+          {spaces.map((space) => (
+            <SpaceCard key={space.id} item={space} />
+          ))}
+        </div>
+      )}
+    </section>
   );
 }
