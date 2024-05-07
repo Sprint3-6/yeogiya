94241488ff2986a692be99f497955eab029676f8
괴도소연\박소연 <rnlduq157@naver.com>
2024-05-07 21:07:49 +0900

    design: 배너 1차 추가 구현과 검색바 css 수정

---
+++ src/pages/Main/components/Banner/index.tsx
@@ -0,0 +1,15 @@
+import './style.scss';
+
+export default function Banner() {
+  return (
+    <div className="main-banner-image">
+      <div className="main-banner-description">
+        <h1>
+          원하는 테마의 공간을 <br />
+          선택하세요!
+        </h1>
+        <p>YEOGIYA와 함께, 다양한 테마의 공간을 등록하고 이용해보세요</p>
+      </div>
+    </div>
+  );
+}
