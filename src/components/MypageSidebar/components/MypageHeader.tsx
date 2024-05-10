0273deddd29e8291c38b995c4218bf13a9fdb1ff
unknown <kimksy27@gmail.com>
2024-05-10 18:45:12 +0900

    Refactor : css 정리

--- src/components/MypageSidebar/components/MypageHeader.tsx
+++ src/components/MypageSidebar/components/MypageHeader.tsx
@@ -1,41 +1,10 @@
 import MypageSideList from './MypageSideList';
 import './MypageHeader.scss';
-import { useRef, useState } from 'react';
 
 export const MypageHeader = () => {
-  const scrollRef = useRef<HTMLDivElement>(null);
-
-  const [isDrag, setIsDrag] = useState(false);
-  const [startX, setStartX] = useState<number>();
-
-  const onDragStart = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setIsDrag(true);
-    setStartX(e.pageX);
-  };
-
-  const onDragEnd = () => {
-    setIsDrag(false);
-  };
-
-  const onDragMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (isDrag && startX !== undefined && scrollRef.current) {
-      scrollRef.current.scrollLeft = startX - e.pageX;
-    }
-  };
-
   return (
     <div className="mypage-header">
-      <div
-        className="mypage-header-box"
-        ref={scrollRef}
-        onMouseDown={onDragStart}
-        onMouseMove={onDragMove}
-        onMouseUp={onDragEnd}
-        onMouseLeave={onDragEnd}
-      >
-        <MypageSideList />
-      </div>{' '}
+      <MypageSideList />
     </div>
   );
 };
