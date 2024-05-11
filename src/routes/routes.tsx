32d4dbbf5bf8e613ab3a927e8a9131032a95ba3a
Jinah <monsatfri@gmail.com>
2024-05-11 19:52:31 +0900

    fix: 불필요 route 제거

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,19 +2,18 @@
 
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
-import SpaceDetails from '@/pages/SpaceDetails';
-import Loading from '@/pages/Loading';
+import AddSpace from '@/pages/AddSpace';
+import EditSpace from '@/pages/EditSpace';
 import MainPage from '@/pages/Main';
+import MyPage from '@/pages/MyPage';
+import MyReservation from '@/pages/MyReservation';
 import MySpaceManagement from '@/pages/MySpaceManagement';
 import NotFound from '@/pages/NotFound';
 import ReservationStatus from '@/pages/ReservationStatus';
 import SignIn from '@/pages/SignIn';
 import SignUp from '@/pages/SignUp';
+import SpaceDetails from '@/pages/SpaceDetails';
 import Layout from './layout';
-import MyPage from '@/pages/MyPage';
-import MyReservation from '@/pages/MyReservation';
-import EditSpace from '@/pages/EditSpace';
-import AddSpace from '@/pages/AddSpace';
 import MypageLayout from './mypagelayout';
 import SignLayout from './signLayout';
 
@@ -32,7 +31,6 @@ const PrimaryRoute = (
         <Route path="mypage" element={<MyPage />} />
       </Route>
     </Route>
-    <Route path="loading" element={<Loading />} />
     <Route element={<SignLayout />}>
       <Route path="sign-up" element={<SignUp />} />
       <Route path="sign-in" element={<SignIn />} />
