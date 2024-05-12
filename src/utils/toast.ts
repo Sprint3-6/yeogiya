26ead4a6a51853433e6c66ad0489cb529bbc0ce1
Park JunYong <dyden119@gmail.com>
2024-05-12 19:41:29 +0900

    chore: 토스트에 상태바 추가

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -7,26 +7,41 @@ const createChild = (text: string, type: string) => {
   const toastContainer = document.createElement('div');
   const toastImg = document.createElement('img');
   const toastText = document.createElement('div');
+  const toastProgressBar = document.createElement('div');
 
   toastImg.src = handleImageSource(type);
   toastText.innerText = text;
+  toastProgressBar.classList.add('progress-bar');
 
   toastContainer.classList.add('toast');
   toastContainer.classList.add(type);
 
   toastContainer.appendChild(toastImg);
   toastContainer.appendChild(toastText);
+  toastContainer.appendChild(toastProgressBar);
 
   toastContainer.addEventListener('click', () => toastContainer.remove());
   root.parentElement?.appendChild(toastContainer);
 
-  setTimeout(() => {
-    toastContainer.classList.add('out');
-  }, 2000);
+  const duration = 2280;
+  const interval = 20;
+  let currentTime = 0;
 
-  setTimeout(() => {
-    toastContainer.remove();
-  }, 2280);
+  toastProgressBar.style.width = '100%';
+
+  const progressBarAnimation = setInterval(() => {
+    const remainingTime = duration - currentTime;
+    const progress = (remainingTime / duration) * 100;
+    toastProgressBar.style.width = `${progress}%`;
+    currentTime += interval;
+    if (currentTime >= duration) {
+      clearInterval(progressBarAnimation);
+      toastContainer.classList.add('out');
+      setTimeout(() => {
+        toastContainer.remove();
+      }, 280);
+    }
+  }, interval);
 };
 
 const toast = {
